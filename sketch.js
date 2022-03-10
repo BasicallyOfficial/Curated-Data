48c976c0c75a1c0c6a47946f28ebf5af5db48d81
BasicallyOfficial <38772185+BasicallyOfficial@users.noreply.github.com>
2022-03-09 17:46:23 -0800

    Added Grunt and Face Movement

--- sketch.js
+++ sketch.js
@@ -1,7 +1,63 @@
+let sound, amplitude;
+let size;
+
+function preload(){
+  sound = loadSound('Assets/2.wav');
+}
+
 function setup() {
-  createCanvas(400, 400);
+    let cnv = createCanvas(500, 500);
+ cnv.mouseClicked(togglePlay);
+   amplitude = new p5.Amplitude();
 }
 
 function draw() {
-  background(220);
+   background(187, 201, 191);
+  let level = amplitude.getLevel();
+  size = map(level, 0, 1, 0, 200);
+  console.log(size);
+  Face();
+}
+
+function Face(){
+
+  //iris
+  push()
+  drawYellowCircle(width*.25, height*.3,width*.11);
+  drawYellowCircle(width*.75, height*.3,width*.11);
+ pop()
+
+  //pupil
+  push()
+  drawBlackCircle(width*.25, height*.3,width*.07,);
+  drawBlackCircle(width*.75, height*.3,width*.07,);
+  pop()
+
+  //mouth
+  push()
+  fill(0)
+   rect(width*.32, height*.52, width*.38, height*.02+(size*1.5), width*.2);
+  pop()
+
+}
+
+function drawBlackCircle(circleX, circleY, circleDiameter){
+  fill(0);
+  circle(circleX, circleY, circleDiameter+(size/4));
+}
+
+
+function drawYellowCircle(circleX, circleY, circleDiameter){
+  fill(180, 189, 51)
+  circle(circleX, circleY, circleDiameter+(size/6))
+}
+
+function togglePlay() {
+  if (sound.isPlaying() ){
+    sound.pause();
+  } else {
+    sound.loop();
+		amplitude = new p5.Amplitude();
+		amplitude.setInput(sound);
+  }
 }
